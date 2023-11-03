0d771b1732b4c4e24b9a13b4bae2be673e7cac9b
Iris System <iris@iris.ac.nz>
2023-11-03 19:34:41 +1300

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+    fu.url = "github:numtide/flake-utils";
+    compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, fu, ... }@inputs: 
+    fu.lib.eachDefaultSystem (system:
+      let
+        pkgs = import inputs.nixpkgs { inherit system; };
+        inherit (pkgs) lib;
+
+        devLibraries = with pkgs; [
+          curl
+        ];
+
+      in {
+        devShells.default = pkgs.mkShell {
+          LD_LIBRARY_PATH = lib.makeLibraryPath devLibraries;
+          buildInputs = devLibraries ++ (with pkgs; [
+            ruby_3_2
+          ]);
+        };
+      });
+}
